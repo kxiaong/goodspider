79760917e285609108a254b40e8f1c9de3dfd58f
kangxiaoling <kangxiaoling@p1.com>
2020-08-04 23:44:27 +0800

    add url

--- goodspider/spiders/zhidemai.py
+++ goodspider/spiders/zhidemai.py
@@ -6,9 +6,8 @@ class ZhidemaiSpider(scrapy.Spider):
     allowed_domains = ["smzdm.com"]
     start_urls = [
             "https://www.smzdm.com/homepage/headline",
+            "https://search.smzdm.com/?c=home&s=%E5%8E%86%E5%8F%B2%E4%BD%8E%E4%BB%B7&v=a"
             ]
 
     def parse(self, response):
         result = json.loads(response.body)
-        print(result)
-        lskfjhlskfuskjdfh
