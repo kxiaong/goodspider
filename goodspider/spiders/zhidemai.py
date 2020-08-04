80329a333bf46301749db4fa9fd7c417b817eec9
kangxiaoling <kangxiaoling@p1.com>
2020-08-04 23:37:33 +0800

    init repo

---
+++ goodspider/spiders/zhidemai.py
@@ -0,0 +1,13 @@
+import scrapy
+import json
+
+class ZhidemaiSpider(scrapy.Spider):
+    name = "zhidemai"
+    allowed_domains = ["smzdm.com"]
+    start_urls = [
+            "https://www.smzdm.com/homepage/headline",
+            ]
+
+    def parse(self, response):
+        result = json.loads(response.body)
+        print(result)
